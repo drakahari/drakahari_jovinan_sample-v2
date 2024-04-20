c428afd588bf5d69c67ca49e0ed9376f1ec8924f
drakahari <164675758+drakahari@users.noreply.github.com>
2024-04-20 20:14:16 +0000

    application being routed to db but not sticking yet

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 
 app = Flask(__name__)
@@ -29,6 +29,21 @@ def show_job(id):
   return render_template('jobpage.html', 
                          job=job)
 
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  
+  # store this data in the database
+  # send an email
+  #display an acknowledgement
+  return render_template('application_submitted.html',
+                         application=data, job=job)
+  
+  ##return jsonify(data)
 ##return jsonify(job)
       
 
