0bf85bd804eddf7756b3ee0396432c97650ce193
drakahari <164675758+drakahari@users.noreply.github.com>
2024-04-20 14:28:26 +0000

    cleaning up database code with python dictionary

--- app.py
+++ app.py
@@ -1,46 +1,19 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
+
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-      'id': 1,
-      'title': 'Data Analyst',
-      'location': 'Bengaluru, India',
-      'salary': 'Rs. 10,00,000'
-  },
-  
-  {
-      'id': 2,
-      'title': 'Data Scientist',
-      'location': 'Delhi, India',
-      'salary': 'Rs. 15,00,000'
-  },
-
-  {
-      'id': 3,
-      'title': 'Frontend Engineer',
-      'location': 'Remote',
-      
-  },
-
-  {
-      'id': 4,
-      'title': 'Backend Engineer',
-      'location': 'San Fransico, USA',
-      'salary': '$120,000'
-  },
 
 
 
-  
-]
 
 @app.route("/")
 def hello_world():
-    return render_template('home.html', 
-                           jobs=JOBS,
-                           company_name='Drakahari')
+  jobs = load_jobs_from_db()
+  return render_template('home.html', 
+                         jobs=jobs,
+                         company_name='Drakahari')
 
 @app.route("/api/jobs")
 def list_jobs():
--- database.py
+++ database.py
@@ -11,16 +11,28 @@ engine = create_engine(
     }
   })
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from jobs"))
-  print("type(result):", type(result))
-  result_all = result.all()
-  print("type(result.all()):" , type(result_all))
-  ##print("result.all():" , result_all)
-  first_result = result_all[0]
+##old code being left as it helps me understand transfer from database to app.py and then back
+  ##print("type(result):", type(result))
+  ##result_all = result.all()
+  ##print("type(result.all()):" , type(result_all))
+  ###print("result.all():" , result_all)
+  ##first_result = result_all[0]
   ##print(type(result_all[0]))
-  print("type(first_result):" , type(first_result))
+  ##print("type(first_result):" , type(first_result))
   ##first_result_dict = dict(result_all[0]) does not work need alternate method
-  first_result_dict = result_all[0]._asdict()
-  print("type(first_result_dict):" , type(first_result_dict))
-  print(first_result_dict)
+  ##first_result_dict = result_all[0]._asdict()
+  ##print("type(first_result_dict):" , type(first_result_dict))
+  ##print(first_result_dict)
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(row._asdict())
+    return jobs
+  ##result_dicts.append(row._mapping)
+
+
+
+##print(result_dicts)
